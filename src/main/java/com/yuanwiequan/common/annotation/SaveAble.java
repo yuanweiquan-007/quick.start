8e1cab28108b1227b8a8e1096184971ae733def7
yuanweiquan <494420676@qq.com>
2020-07-16 14:30:04 +0800

    新增util

--- src/main/java/com/yuanwiequan/common/annotation/SaveAble.java
+++ src/main/java/com/yuanwiequan/common/annotation/SaveAble.java
@@ -2,8 +2,11 @@ package com.yuanwiequan.common.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * 用来控制是否保存
+ */
 @Documented
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface UnSave {
+public @interface SaveAble {
 }
