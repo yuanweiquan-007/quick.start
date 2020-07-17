43e24a6c3f4af0bd2b17d424024b11392ca2e43c
yuanweiquan <18146605067@163.com>
2020-07-17 21:43:59 +0800

    新增主键和表名检查

--- src/main/java/quick/start/repository/DefaultAbstractRepository.java
+++ src/main/java/quick/start/repository/DefaultAbstractRepository.java
@@ -1,53 +1,45 @@
 package quick.start.repository;
 
-import quick.start.entity.Entity;
-import quick.start.entity.EntityMeta;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
+import quick.start.entity.Entity;
+import quick.start.entity.EntityMeta;
 import quick.start.parser.CommandParser;
-import quick.start.repository.command.Delete;
-import quick.start.repository.command.Insert;
+import quick.start.repository.command.CommandFactory;
+import quick.start.repository.command.CommandForEntity;
 import quick.start.repository.command.Select;
-import quick.start.repository.command.Update;
 
-import java.lang.reflect.ParameterizedType;
+import java.io.Serializable;
+import java.util.List;
 
 public abstract class DefaultAbstractRepository<E extends Entity> implements Repository<E> {
 
-     protected EntityMeta entityMeta;
      protected Logger logger = LoggerFactory.getLogger(getClass());
-
-     public DefaultAbstractRepository() {
-          initEntityMeta();
-     }
-
-     /**
-      * 获取泛型类的类型
-      *
-      * @return
-      */
-     protected Class<E> entityClass() {
-          return (Class<E>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-     }
-
-     protected void checkPrimaryKey() {
-          Assert.notNull(entityMeta.getPrimaryKey(), "primaryKey未设置#可以使用@PrimaryKey注解来设置");
-     }
-
-     protected void checkTableName() {
-          Assert.notNull(entityMeta.getTableName(), "tableName未设置#可以通过注解@Table注解来设置");
-     }
-
-     protected void checkTableAndPrimaryKey() {
-          checkTableName();
-          checkPrimaryKey();
-     }
-
-     protected void initEntityMeta() {
-          entityMeta = EntityMeta.of(entityClass());
-     }
+     protected CommandFactory<E> commandFactory = new CommandFactory<>();
 
      protected abstract CommandParser commandParser();
+
+     protected abstract List<E> select(Select<E> select);
+
+     @Override
+     public E findById(Serializable id) {
+          Select<E> select = commandFactory.select();
+          checkPrimaryKeyAndTableName(select);
+          return null;
+     }
+
+     protected void checkTableName(CommandForEntity<E> commandForEntity) {
+          Assert.notNull(commandForEntity.getMeta().getTableName(), "未设置表名，可通过注解@Table来定义表名");
+     }
+
+     protected void checkPrimaryKey(CommandForEntity<E> commandForEntity) {
+          Assert.notNull(commandForEntity.getMeta().getPrimaryKey(), "未设置主键，可通过注解@PrimaryKey来定义主键");
+     }
+
+     protected void checkPrimaryKeyAndTableName(CommandForEntity<E> commandForEntity) {
+          checkPrimaryKey(commandForEntity);
+          checkTableName(commandForEntity);
+     }
 
 }
--- src/main/java/quick/start/repository/jdbc/JdbcRepository.java
+++ src/main/java/quick/start/repository/jdbc/JdbcRepository.java
@@ -1,104 +1,125 @@
 package quick.start.repository.jdbc;
 
+import org.springframework.util.StringUtils;
 import quick.start.entity.Entity;
 import quick.start.parser.CommandParser;
 import quick.start.parser.jdbcparser.JdbcCommandParser;
 import quick.start.repository.DefaultAbstractRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import quick.start.repository.command.ExecuteCommandMeta;
+import quick.start.repository.command.Select;
+import quick.start.util.EntityMapper;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class JdbcRepository<E extends Entity> extends DefaultAbstractRepository<E> {
 
-     @Autowired
-     private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-     @Override
-     public E findById(Serializable id) {
-          return null;
-     }
+    @Override
+    protected List<E> select(Select<E> select) {
+        checkTableName(select.getMeta());
+        ExecuteCommandMeta commandMeta = commandParser().parser(select);
+        if (StringUtils.isEmpty(commandMeta.getCommand())) {
+            throw new IllegalArgumentException("select语句解析异常");
+        }
+        try {
+            return EntityMapper.toEntityList(this.jdbcTemplate.queryForList(commandMeta.getCommand(), commandMeta.getParames().toArray()), select.getMeta().getEClass());
+        } catch (Exception e) {
+            logger.info("查询失败", e);
+            return new ArrayList<>();
+        }
+    }
 
-     @Override
-     public List<E> findByIds(Collection<? extends Serializable> ids) {
-          return null;
-     }
+    @Override
+    public E findById(Serializable id) {
+        return null;
+    }
 
-     @Override
-     public List<E> findByColumn(String column, Serializable value) {
-          return null;
-     }
+    @Override
+    public List<E> findByIds(Collection<? extends Serializable> ids) {
+        return null;
+    }
 
-     @Override
-     public List<E> findByColumn(String column, Collection<? extends Serializable> values) {
-          return null;
-     }
+    @Override
+    public List<E> findByColumn(String column, Serializable value) {
+        return null;
+    }
 
-     @Override
-     public List<E> find() {
-          return null;
-     }
+    @Override
+    public List<E> findByColumn(String column, Collection<? extends Serializable> values) {
+        return null;
+    }
 
-     @Override
-     public boolean has(Serializable id) {
-          return false;
-     }
+    @Override
+    public List<E> find() {
+        return null;
+    }
 
-     @Override
-     public boolean insert(E entity) {
-          return false;
-     }
+    @Override
+    public boolean has(Serializable id) {
+        return false;
+    }
 
-     @Override
-     public boolean insert(List<E> entitys) {
-          return false;
-     }
+    @Override
+    public boolean insert(E entity) {
+        return false;
+    }
 
-     @Override
-     public boolean update(E entity) {
-          return false;
-     }
+    @Override
+    public boolean insert(List<E> entitys) {
+        return false;
+    }
 
-     @Override
-     public boolean update(List<? extends Serializable> ids, Map<String, Object> data) {
-          return false;
-     }
+    @Override
+    public boolean update(E entity) {
+        return false;
+    }
 
-     @Override
-     public boolean update(String id, Map<String, Object> data) {
-          return false;
-     }
+    @Override
+    public boolean update(List<? extends Serializable> ids, Map<String, Object> data) {
+        return false;
+    }
 
-     @Override
-     public boolean update(String id, String key, Object value) {
-          return false;
-     }
+    @Override
+    public boolean update(String id, Map<String, Object> data) {
+        return false;
+    }
 
-     @Override
-     public boolean update(List<? extends Serializable> ids, String key, Object value) {
-          return false;
-     }
+    @Override
+    public boolean update(String id, String key, Object value) {
+        return false;
+    }
 
-     @Override
-     public boolean delete(Serializable id) {
-          return false;
-     }
+    @Override
+    public boolean update(List<? extends Serializable> ids, String key, Object value) {
+        return false;
+    }
 
-     @Override
-     public boolean delete(List<? extends Serializable> ids) {
-          return false;
-     }
+    @Override
+    public boolean delete(Serializable id) {
+        return false;
+    }
 
-     @Override
-     public boolean delete(String column, Collection<? extends Serializable> values) {
-          return false;
-     }
+    @Override
+    public boolean delete(List<? extends Serializable> ids) {
+        return false;
+    }
 
-     @Override
-     protected CommandParser commandParser() {
-          return new JdbcCommandParser();
-     }
+    @Override
+    public boolean delete(String column, Collection<? extends Serializable> values) {
+        return false;
+    }
+
+    @Override
+    protected CommandParser commandParser() {
+        return new JdbcCommandParser();
+    }
+
 }
