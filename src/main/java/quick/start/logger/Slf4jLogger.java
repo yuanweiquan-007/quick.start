c5741ca2328b06835c9718fa4ba109e54e236cd8
yuanweiquan <18146605067@163.com>
2020-08-08 19:41:34 +0800

    新增默认log实现Slf4jLogger

---
+++ src/main/java/quick/start/logger/Slf4jLogger.java
@@ -0,0 +1,13 @@
+package quick.start.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author yuanweiquan
+ */
+public class Slf4jLogger {
+
+     protected Logger logger = LoggerFactory.getLogger(getClass());
+
+}
