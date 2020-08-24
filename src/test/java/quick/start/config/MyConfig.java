cc0f05c5dcd8f38353829713de786c6207bbf2fb
yuanweiquan <18146605067@163.com>
2020-08-24 15:28:49 +0800

    移除测试mysql链接

--- src/test/java/quick/start/config/MyConfig.java
+++ src/test/java/quick/start/config/MyConfig.java
@@ -18,23 +18,13 @@ public class MyConfig {
           return jdbcTemplate;
      }
 
-//     @Bean
-//     public DataSource dataSource() {
-//          BasicDataSource dataSource = new BasicDataSource();
-//          dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-//          dataSource.setUsername("root");
-//          dataSource.setPassword("root");
-//          dataSource.setUrl("jdbc:mysql://localhost:3306/spring_study?zeroDateTimeBehavior=convertToNull");
-//          return dataSource;
-//     }
-
      @Bean
      public DataSource dataSource() {
           BasicDataSource dataSource = new BasicDataSource();
           dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-          dataSource.setUsername("ywq");
-          dataSource.setPassword("bcw@ywq123456");
-          dataSource.setUrl("jdbc:mysql://122.224.240.99:10003/ecall_core_archives?zeroDateTimeBehavior=convertToNull");
+          dataSource.setUsername("root");
+          dataSource.setPassword("root");
+          dataSource.setUrl("jdbc:mysql://localhost:3306/spring_study?zeroDateTimeBehavior=convertToNull");
           return dataSource;
      }
 
