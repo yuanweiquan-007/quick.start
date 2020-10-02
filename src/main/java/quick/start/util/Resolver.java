b474d48d1ee99f27a94c0e8b395a8a58e8185e49
yuanweiquan <18146605067@163.com>
2020-10-02 17:11:46 +0800

    resolver新增ifPresent

--- src/main/java/quick/start/util/Resolver.java
+++ src/main/java/quick/start/util/Resolver.java
@@ -8,7 +8,7 @@ import quick.start.entity.Entity;
 import quick.start.entity.EntityMapper;
 
 import java.util.*;
-import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 /**
  * 解析器
@@ -17,108 +17,115 @@ import java.util.function.BiConsumer;
  */
 public class Resolver {
 
-     protected final Map<String, Object> data = new LinkedHashMap<>();
+    protected final Map<String, Object> data = new LinkedHashMap<>();
 
-     public final static String DELIMITERS = "|-/#;,: ";
+    public final static String DELIMITERS = "|-/#;,: ";
 
-     private Resolver() {
-     }
+    private Resolver() {
+    }
 
-     private Resolver(Map<String, ?> data) {
-          if (data != null) {
-               data.forEach(this::set);
-          }
-     }
+    private Resolver(Map<String, ?> data) {
+        if (data != null) {
+            data.forEach(this::set);
+        }
+    }
 
-     public static Resolver of() {
-          return new Resolver();
-     }
+    public static Resolver of() {
+        return new Resolver();
+    }
 
-     public static Resolver of(Map<String, ?> data) {
-          return new Resolver(data);
-     }
+    public static Resolver of(Map<String, ?> data) {
+        return new Resolver(data);
+    }
 
-     public static Resolver of(String key, Object value) {
-          Resolver serviceForm = new Resolver();
-          serviceForm.set(key, value);
-          return serviceForm;
-     }
+    public static Resolver of(String key, Object value) {
+        Resolver serviceForm = new Resolver();
+        serviceForm.set(key, value);
+        return serviceForm;
+    }
 
-     public Resolver set(String key, Object value) {
-          data.put(key, value);
-          return this;
-     }
+    public Resolver set(String key, Object value) {
+        data.put(key, value);
+        return this;
+    }
 
-     public static Resolver ofJson(String json) throws Exception {
-          return of(JsonUtils.toMap(json));
-     }
+    public static Resolver ofJson(String json) throws Exception {
+        return of(JsonUtils.toMap(json));
+    }
 
-     public static Resolver ofXml(String xml) throws Exception {
-          return of(XmlUtils.toMap(xml));
-     }
+    public static Resolver ofXml(String xml) throws Exception {
+        return of(XmlUtils.toMap(xml));
+    }
 
-     public static Resolver ofXml(String xml, String root) throws Exception {
-          Map<String, Object> data = XmlUtils.toMap(xml);
-          if (CollectionUtils.isEmpty(data) || !data.containsKey(root)) {
-               return of();
-          }
-          Object tmp = data.get(root);
-          if (ObjectUtils.isEmpty(tmp) && !Map.class.isInstance(tmp)) {
-               return of();
-          }
-          return of((Map<String, Object>) tmp);
-     }
+    public static Resolver ofXml(String xml, String root) throws Exception {
+        Map<String, Object> data = XmlUtils.toMap(xml);
+        if (CollectionUtils.isEmpty(data) || !data.containsKey(root)) {
+            return of();
+        }
+        Object tmp = data.get(root);
+        if (ObjectUtils.isEmpty(tmp) && !Map.class.isInstance(tmp)) {
+            return of();
+        }
+        return of((Map<String, Object>) tmp);
+    }
 
-     public static <E extends Entity> Resolver ofEntity(E entity) {
-          return of(EntityMapper.toMap(entity));
-     }
+    public static <E extends Entity> Resolver ofEntity(E entity) {
+        return of(EntityMapper.toMap(entity));
+    }
 
-     public <T> T get(String key) {
-          Object value = getValue(key);
-          if (!ObjectUtils.isEmpty(value)) {
-               return (T) value;
-          } else {
-               return null;
-          }
-     }
+    public <T> T get(String key) {
+        Object value = getValue(key);
+        if (!ObjectUtils.isEmpty(value)) {
+            return (T) value;
+        } else {
+            return null;
+        }
+    }
 
-     private Object getValue(String key) {
-          Assert.hasLength(key, "key invalid");
-          String[] tokenizeToStringArray = StringUtils.tokenizeToStringArray(key, DELIMITERS);
-          Map<String, Object> currentData = this.data;
-          for (int i = 0; i < tokenizeToStringArray.length; i++) {
-               String token = tokenizeToStringArray[i];
-               if (!currentData.containsKey(token)) {
+    public <T> void ifPresent(String key, Consumer<T> consumer) {
+        T result = get(key);
+        if (!ObjectUtils.isEmpty(result)) {
+            consumer.accept(result);
+        }
+    }
+
+    private Object getValue(String key) {
+        Assert.hasLength(key, "key invalid");
+        String[] tokenizeToStringArray = StringUtils.tokenizeToStringArray(key, DELIMITERS);
+        Map<String, Object> currentData = this.data;
+        for (int i = 0; i < tokenizeToStringArray.length; i++) {
+            String token = tokenizeToStringArray[i];
+            if (!currentData.containsKey(token)) {
+                return null;
+            }
+            if (i == tokenizeToStringArray.length - 1) {
+                return currentData.get(token);
+            } else {
+                Object currentValue = currentData.get(token);
+                if (Map.class.isInstance(currentValue)) {
+                    currentData = (Map<String, Object>) currentValue;
+                } else {
                     return null;
-               }
-               if (i == tokenizeToStringArray.length - 1) {
-                    return currentData.get(token);
-               } else {
-                    Object currentValue = currentData.get(token);
-                    if (Map.class.isInstance(currentValue)) {
-                         currentData = (Map<String, Object>) currentValue;
-                    } else {
-                         return null;
-                    }
-               }
-          }
-          return null;
-     }
+                }
+            }
+        }
+        return null;
+    }
 
-     public <T> List<T> getList(String key) {
-          List<T> container = new ArrayList<>();
-          Object value = get(key);
-          if (!ObjectUtils.isEmpty(value)) {
-               if (value instanceof Collection) {
-                    container.addAll((Collection<? extends T>) value);
-               } else {
-                    container.add((T) value);
-               }
-          }
-          return container;
-     }
+    public <T> List<T> getList(String key) {
+        List<T> container = new ArrayList<>();
+        Object value = get(key);
+        if (!ObjectUtils.isEmpty(value)) {
+            if (value instanceof Collection) {
+                container.addAll((Collection<? extends T>) value);
+            } else {
+                container.add((T) value);
+            }
+        }
+        return container;
+    }
 
-     public Map<String, Object> getData() {
-          return data;
-     }
+    public Map<String, Object> getData() {
+        return data;
+    }
 }
