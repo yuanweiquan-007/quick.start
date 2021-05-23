8321940c43aafaa86a7f46a0f09ddff6656e1f31
九歌 <494420676@qq.com>
2021-05-23 15:42:44 +0800

    StringUtils新增isNotEmpty方法

--- src/main/java/quick/start/util/StringUtils.java
+++ src/main/java/quick/start/util/StringUtils.java
@@ -1,5 +1,7 @@
 package quick.start.util;
 
+import org.springframework.lang.Nullable;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,57 +10,61 @@ import java.util.regex.Pattern;
  */
 public class StringUtils extends org.springframework.util.StringUtils {
 
-     private static Pattern humpPattern = Pattern.compile("[A-Z]");
+    private static Pattern humpPattern = Pattern.compile("[A-Z]");
 
-     public static String concat(String delimiter, String... strings) {
-          return StreamUtils
-                  .of(strings)
-                  .reduce((a, b) -> a + delimiter + b)
-                  .get();
-     }
+    public static Boolean isNotEmpty(@Nullable Object str) {
+        return !isEmpty(str);
+    }
 
-     /**
-      * 首字母转大写
-      *
-      * @param str 字符串
-      * @return 首字母大写字符串
-      */
-     public static String toUpperCaseFirstOne(String str) {
-          if (Character.isUpperCase(str.charAt(0))) {
-               return str;
-          } else {
-               return (new StringBuilder()).append(Character.toUpperCase(str.charAt(0))).append(str.substring(1)).toString();
-          }
-     }
+    public static String concat(String delimiter, String... strings) {
+        return StreamUtils
+                .of(strings)
+                .reduce((a, b) -> a + delimiter + b)
+                .get();
+    }
 
-     /**
-      * 首字母转小写
-      *
-      * @param str 字符串
-      * @return 首字母小写字符串
-      */
-     public static String toLowerCaseFirstOne(String str) {
-          if (Character.isLowerCase(str.charAt(0))) {
-               return str;
-          } else {
-               return (new StringBuilder()).append(Character.toLowerCase(str.charAt(0))).append(str.substring(1)).toString();
-          }
-     }
+    /**
+     * 首字母转大写
+     *
+     * @param str 字符串
+     * @return 首字母大写字符串
+     */
+    public static String toUpperCaseFirstOne(String str) {
+        if (Character.isUpperCase(str.charAt(0))) {
+            return str;
+        } else {
+            return (new StringBuilder()).append(Character.toUpperCase(str.charAt(0))).append(str.substring(1)).toString();
+        }
+    }
 
-     /**
-      * 驼峰转下划线
-      *
-      * @param str 字符串
-      * @return 驼峰字符串
-      */
-     public static String humpToLine(String str) {
-          Matcher matcher = humpPattern.matcher(toLowerCaseFirstOne(str));
-          StringBuffer stringBuffer = new StringBuffer();
-          while (matcher.find()) {
-               matcher.appendReplacement(stringBuffer, "_" + matcher.group(0).toLowerCase());
-          }
-          matcher.appendTail(stringBuffer);
-          return stringBuffer.toString();
-     }
+    /**
+     * 首字母转小写
+     *
+     * @param str 字符串
+     * @return 首字母小写字符串
+     */
+    public static String toLowerCaseFirstOne(String str) {
+        if (Character.isLowerCase(str.charAt(0))) {
+            return str;
+        } else {
+            return (new StringBuilder()).append(Character.toLowerCase(str.charAt(0))).append(str.substring(1)).toString();
+        }
+    }
+
+    /**
+     * 驼峰转下划线
+     *
+     * @param str 字符串
+     * @return 驼峰字符串
+     */
+    public static String humpToLine(String str) {
+        Matcher matcher = humpPattern.matcher(toLowerCaseFirstOne(str));
+        StringBuffer stringBuffer = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(stringBuffer, "_" + matcher.group(0).toLowerCase());
+        }
+        matcher.appendTail(stringBuffer);
+        return stringBuffer.toString();
+    }
 
 }
