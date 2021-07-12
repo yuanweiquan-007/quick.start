45e1107c88400ba6790dfba29971451cb41f77df
九歌 <494420676@qq.com>
2021-07-12 19:57:48 +0800

    移除final

--- src/main/java/quick/start/util/StringBufferUtils.java
+++ src/main/java/quick/start/util/StringBufferUtils.java
@@ -5,7 +5,7 @@ package quick.start.util;
  */
 public class StringBufferUtils {
 
-     public static final StringBuffer of() {
+     public static StringBuffer of() {
           return new StringBuffer();
      }
 
