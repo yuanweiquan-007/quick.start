a1b554c2e824b9c0788450b474ea42dbefd4921e
九歌 <494420676@qq.com>
2021-07-14 14:56:13 +0800

    添加注释

--- src/main/java/quick/start/util/DateUtils.java
+++ src/main/java/quick/start/util/DateUtils.java
@@ -13,61 +13,61 @@ import java.util.Date;
  */
 public class DateUtils {
 
-     private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-     /**
-      * 格式化Date
-      *
-      * @param date
-      * @return
-      */
-     public static String format(Date date) {
-          return SIMPLE_DATE_FORMAT.format(date);
-     }
+    /**
+     * 格式化Date
+     *
+     * @param date
+     * @return
+     */
+    public static String format(Date date) {
+        return SIMPLE_DATE_FORMAT.format(date);
+    }
 
-     /**
-      * 字符串转Date
-      *
-      * @param date
-      * @return
-      * @throws ParseException
-      */
-     public static Date parser(String date) throws ParseException {
-          synchronized (SIMPLE_DATE_FORMAT) {
-               return SIMPLE_DATE_FORMAT.parse(date);
-          }
-     }
+    /**
+     * 字符串转Date
+     *
+     * @param date
+     * @return
+     * @throws ParseException
+     */
+    public static Date parser(String date) throws ParseException {
+        synchronized (SIMPLE_DATE_FORMAT) {
+            return SIMPLE_DATE_FORMAT.parse(date);
+        }
+    }
 
-     /**
-      * 格式化LocalDateTime
-      *
-      * @param localDateTime
-      * @return
-      */
-     public static String format(LocalDateTime localDateTime) {
-          return localDateTime.format(DATE_TIME_FORMATTER);
-     }
+    /**
+     * 格式化LocalDateTime
+     *
+     * @param localDateTime
+     * @return
+     */
+    public static String format(LocalDateTime localDateTime) {
+        return localDateTime.format(DATE_TIME_FORMATTER);
+    }
 
-     /**
-      * date 转 LocalDateTime
-      *
-      * @param date data对象
-      * @return
-      */
-     public static LocalDateTime toLocalDateTime(Date date) {
-          return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
-     }
+    /**
+     * date 转 LocalDateTime
+     *
+     * @param date data对象
+     * @return
+     */
+    public static LocalDateTime toLocalDateTime(Date date) {
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
 
-     /**
-      * LocalDateTime转Date
-      *
-      * @param localDateTime localDateTime对象
-      * @return
-      */
-     public static Date toDate(LocalDateTime localDateTime) {
-          return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-     }
+    /**
+     * LocalDateTime转Date
+     *
+     * @param localDateTime localDateTime对象
+     * @return Date对象
+     */
+    public static Date toDate(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
 
 
 }
